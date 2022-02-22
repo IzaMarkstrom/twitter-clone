5936b8bcac489c3e5572a80857116303059c5ba5
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-02-22 21:45:47 +0100

    Added code to check if client is logged in or not

--- middleware.js
+++ middleware.js
@@ -1,7 +1,10 @@
 exports.requireLogin = (req, res, next) => {
+    // Checks if user is logged in.
     if (req.session && req.session.user){
+        // If so then we use next
         return next()
     } else {
+        // If user is not logged in we redirect them to login page.
         return res.redirect("/login")
     }
 }

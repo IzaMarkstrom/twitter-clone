61cb8204a9f20d78a0edf7a2c16e1dd9cec4bd27
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-02-24 17:45:11 +0100

    Added register page.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express()
 const PORT = 3000;
 const middleware = require("./middleware")
+const path = require("path")
 
 // Telling express to listen to port 3000.
 const server = app.listen(PORT, () => {
@@ -13,10 +14,15 @@ app.set("view engine", "pug")
 // When you need views, go to folder namned views.
 app.set("views", "views")
 
+// If something might change in the path this is the safer way to write. 
+app.use(express.static(path.join(__dirname, "public")))
+
 // Routes
 const loginRoute = require("./routes/loginRoutes")
+const registerRoute = require("./routes/registerRoutes")
 
 app.use("/login", loginRoute)
+app.use("/register", registerRoute)
 
 // Adding req = request from client and res = response from server parameter.
 app.get("/", middleware.requireLogin, (req, res, next) => {

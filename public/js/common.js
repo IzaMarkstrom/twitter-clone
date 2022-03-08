6d257405cab8186b8adb1c32d50da86aaa929636
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-03-08 10:06:02 +0100

    Added html for posts and time calculation

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,8 @@
 // Function to enable and disabled the POST button. 
 $("#postTextarea").keyup(event => {
-    var input = $(event.target)
+    var textbox = $(event.target)
     // Trim the value so the user is not able to print a post with only spaces.
-    var value = input.val().trim()
+    var value = textbox.val().trim()
 
     var submitBtn = $("#submitPostBtn")
 
@@ -22,9 +22,95 @@ $("#submitPostBtn").click(() => {
     var data = {
         content: textbox.val()
     }
-
-    // Sending the data to the url and adding a callback function.
-    $.post("/api/posts", data, (postData, status, xhr) => {
-        alert(postData)
+   // Sending the data to the url and adding a callback function.
+    $.post("/api/posts", data, postData => {
+        
+        var html = createPostHtml(postData);
+        $(".postContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
     })
 })
+
+function createPostHtml(postData) {
+
+    var postedBy = postData.postedBy
+
+    if(postedBy._id === undefined){
+        return console.log("User object not populated.")
+    }
+
+    var displayName = postedBy.username
+    var timestamp = timeDifference(new Date(), new Date(postData.createdAt))
+
+    return `<div class="post">
+                <div class="mainContainer">
+                    <div class="userImageContainer">
+                        <img src="${postedBy.profilePic}">
+                    </div>
+                    <div class="postContentContainer">
+                        <div class="header">
+                            <a href="/profile/${postedBy.username}">${displayName}</a>
+                            <span class="date">${timestamp}</span>
+                        </div>
+                        <div class="postBody">
+                            <span>${postData.textContent}</span>
+                        </div>
+                        <div class="postFooter">
+                            <div>
+                                <button>
+                                    <i class="fa-solid fa-comment"></i>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <i class="fa-solid fa-retweet"></i>
+                                </button>
+                            </div>
+                            <div>
+                                <button>
+                                    <i class="fa-solid fa-heart"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        if(elapsed/1000 < 30) return "Just now"
+
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}

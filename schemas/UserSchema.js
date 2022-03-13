1fe9b43a536c911198fd5c0cc0727d8ab1cc72f2
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-03-13 18:21:26 +0100

    Added information to User Schema

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -4,10 +4,15 @@ const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
     username: {type: String, require: true, trim: true, unique: true},
+    firstname: {type: String},
+    lastname: {type: String},
+    email: {type: String, unique: true},
     password: {type: String, require: true},
     profilePic: {type: String, default: "/images/profilePic.jpg"},
     likes: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+    retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }]
 }, { timestamps: true })
 // This will give a timestamp on every document inserted in this collection.
 

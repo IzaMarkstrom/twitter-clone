a0d62266d3818884b1d3f2ff355c5291f7f12708
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-03-13 23:07:53 +0100

    Added comments

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
+// requireLogin function
 exports.requireLogin = (req, res, next) => {
-    // Checks if user is logged in.
+    // Checks if session property is set and if the user property is set. 
     if (req.session && req.session.user){
         // If so then we use next
         return next()
